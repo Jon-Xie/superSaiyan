ea653b937b024ed9d59a30521dea88c3fae5789e
Hack Reactor Student <student@hackreactor.com>
2019-08-01 14:50:00 -0700

    update v2

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -4,11 +4,11 @@ var VideoListEntry = (props) => (
       <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt="" />
     </div>
     <div className="media-body">
-      <div className="video-list-entry-title" onClick={() props.onClickHandler()}>
-      {props.video.snippet.title}
+      <div className="video-list-entry-title" onClick={() => props.onClickHandler(props.video)}>
+        {props.video.snippet.title}
+      </div>
+      <div className="video-list-entry-detail">{props.video.snippet.description}</div>
     </div>
-    <div className="video-list-entry-detail">{props.video.snippet.description}</div>
-  </div>
   </div >
 );
 
@@ -21,3 +21,4 @@ VideoListEntry.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default VideoListEntry;
+
