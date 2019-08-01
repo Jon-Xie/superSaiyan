d7b85f9eeb89550d7ba62e60973903433be30ac6
Hack Reactor Student <student@hackreactor.com>
2019-07-31 20:39:01 -0700

    super saiyan

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// TODO: Render the `App` component to the DOM
+import App from './components/App.js';
+// import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
